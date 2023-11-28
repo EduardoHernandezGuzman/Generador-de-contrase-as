bccd71a83f34c02efd2306a9d5bb405acf2646b6
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-11-28 11:56:41 +0000

    Añadida funcionalidad para alegir la longitud de la contraseña

--- src/js/component/contrasena.jsx
+++ src/js/component/contrasena.jsx
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 
 const Contraseña = () => {
     const [contraseñaGenerada, setContraseñaGenerada] = useState("");
+    const [longitud , setLongitud] = useState(9);
 
-    const letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    const letras = [
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
         "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+        "!", "@", "#", "$", "%", "&", "*", "+", "-"
+      ];
 
     function generarContraseña() {
+        
         let nuevaContraseña = "";
 
-        for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < longitud; i++) {
             const mezclaLetras = letras[Math.floor(Math.random() * letras.length)];
             nuevaContraseña += mezclaLetras;
         }
@@ -19,10 +25,21 @@ const Contraseña = () => {
         setContraseñaGenerada(nuevaContraseña);
     }
 
+    function longitudContraseña(e) {
+        const valor = parseInt(e.target.value);
+        setLongitud(valor);
+    }
+
     return (
         <>
             <h1>Generador de contraseña</h1>
             <hr />
+            <br />
+            <input 
+            type="number"
+            onChange={(e)=> longitudContraseña(e)}
+             /> 
+            <label htmlFor="">Longitud de tu contraseña</label>
             <br />
             <button onClick={generarContraseña}>Generar</button>
             <br />
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Contraseña from "./contraseña";
+import Contraseña from "./contrasena";
 
 
 
